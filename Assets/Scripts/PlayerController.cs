7ac215bbf2b50c7fed6ad6794009d0ca63521666
Hugo B <hugo.brown8@education.nsw.gov.au>
2025-03-06 09:39:24 +1100

    pickupfixandaddcount

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
  // Rigidbody of the player.
  private Rigidbody rb; 
+ private int count;
 
  // Movement along X and Y axes.
  private float movementX;
@@ -15,12 +17,18 @@ public class PlayerController : MonoBehaviour
 
  // Speed at which the player moves.
  public float speed = 0; 
+ public TextMeshProUGUI countText;
+
+ public GameObject winTextObject;
 
  // Start is called before the first frame update.
  void Start()
     {
  // Get and store the Rigidbody component attached to the player.
+        winTextObject.SetActive(false);
+        count = 0; 
         rb = GetComponent<Rigidbody>();
+        SetCountText();
     }
  
  // This function is called when a move input is detected.
@@ -33,21 +41,30 @@ public class PlayerController : MonoBehaviour
         movementX = movementVector.x; 
         movementY = movementVector.y; 
     }
-
+  void SetCountText() 
+   {
+       countText.text =  "Count: " + count.ToString();
+         if (count >= 11)
+       {
+           winTextObject.SetActive(true);
+       }
+   }
  // FixedUpdate is called once per fixed frame-rate frame.
  private void FixedUpdate() 
     {
  // Create a 3D movement vector using the X and Y inputs.
-        Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
+        Vector3 movement = new Vector3 (movementX, 0.1f, movementY);
 
  // Apply force to the Rigidbody to move the player.
         rb.AddForce(movement * speed); 
     }
      private void OnTriggerEnter (Collider other) 
    {
-       if (other.gameObject.CompareTag("PickUp")) 
+       if (other.gameObject.CompareTag("Pickup")) 
        {
            other.gameObject.SetActive(false);
+           count = count + 1;
+           SetCountText();
        }
    }
 }
